d02b941d75776e787d74119be5be4429be3bd02c
zdd <18252113706@163.com>
2022-11-11 20:49:48 +0800

    后端项目搭建与用户登录功能实现

---
+++ backend/src/main/java/com/kob/backend/controller/user/UserController.java
@@ -0,0 +1,9 @@
+package com.kob.backend.controller.user;
+
+import com.kob.backend.pojo.User;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+}
